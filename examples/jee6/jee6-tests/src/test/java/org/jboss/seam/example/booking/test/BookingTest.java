2dc445638fc2a1dd4c7420f4356addeb1220987e
Marek Schmidt <maschmid@redhat.com>
2013-03-01 15:14:00 +0100

    not much

--- examples/jee6/jee6-tests/src/test/java/org/jboss/seam/example/booking/test/BookingTest.java
+++ examples/jee6/jee6-tests/src/test/java/org/jboss/seam/example/booking/test/BookingTest.java
@@ -43,6 +43,8 @@ public class BookingTest
    {
       return Deployments.bookingDeployment();
    }
+   
+   
 
    @Before
    public void before() {
@@ -54,6 +56,8 @@ public class BookingTest
       Lifecycle.endCall();
    }
 
+   
+   
    @Test
    public void testBookHotel() throws Exception
    {
